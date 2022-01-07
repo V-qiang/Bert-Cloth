aca67029e24ed2a0c2d209da93f09853edf6ba1f
窗台的猫 <v_qiang@qq.com>
2022-01-07 15:43:11 +0800

    似懂非懂

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import os
+import os  
 import shutil
 import argparse
 import random
